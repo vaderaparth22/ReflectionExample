9e33184031329f9916b920be3f2d5d9041c57569
vaderaparth22 <vaderaparth22@gmail.com>
2020-12-21 22:45:48 -0500

    Initial commit

---
+++ Assets/Scripts/MainFlow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+using System;
+
+public class MainFlow : MonoBehaviour
+{
+    Dictionary<string, MethodInfo> methodDict = new Dictionary<string, MethodInfo>();
+
+    void Start()
+    {
+        System.Type ourType = this.GetType();
+
+        MethodInfo[] mi = ourType.GetMethods();
+
+        foreach (MethodInfo item in mi)
+        {
+            item.Invoke(this, null);
+        }
+    }
+
+    public void Move()
+    {
+        Debug.Log("moving...");
+    }
+
+    public void Speak()
+    {
+        Debug.Log("speaking...");
+    }
+}
